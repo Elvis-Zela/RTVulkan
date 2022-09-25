b44850b82e3a48e41fb4af1a71d0537e856a2c15
Elvis-Zela <113384118+Elvis-Zela@users.noreply.github.com>
2022-09-25 17:35:06 +0100

    Renderer supporting World
    Updated Renderer to work with new Ray, hittables and world class

--- RayTracerNEA/src/Renderer.h
+++ RayTracerNEA/src/Renderer.h
@@ -2,11 +2,14 @@
 
 #include "Walnut/Image.h"
 
-#include "Camera.h"
-#include "Ray.h"
-
 #include <memory>
 #include <glm/glm.hpp>
+
+#include "Utilities.h"
+
+class Camera;
+class Hittables;
+class Ray;
 
 class Renderer
 {
@@ -14,14 +17,14 @@ public:
 /* - Constructor - */
 	Renderer() = default;
 
-	void Render(const Camera& camera);
-	void Resizing(uint32_t width, uint32_t height);
+	void Render(const Hittables& world, const Camera& camera);
+	void IfResizing(uint32_t width, uint32_t height);
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
 
 /* - Private Methods - */
 private:
-	glm::vec4 TraceRay(const Ray& ray);
+	glm::vec4 TraceRay(const Ray& ray, const Hittables& world);
 
 /* - Attributes - */
 private:
