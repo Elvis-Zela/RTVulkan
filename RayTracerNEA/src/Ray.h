25263c1d0307da0ce6042082cc6891c0bec0f27b
Elvis-Zela <113384118+Elvis-Zela@users.noreply.github.com>
2022-09-25 17:31:06 +0100

    Ray Class Change
    Changed ray from a struct to a class
    Added getters and setters
    Added At() method to find point given a t value

--- RayTracerNEA/src/Ray.h
+++ RayTracerNEA/src/Ray.h
@@ -2,8 +2,24 @@
 
 #include <glm/glm.hpp>
 
-struct Ray
+class Ray
 {
-	glm::vec3 Origin;
-	glm::vec3 Direction;
+public:
+	/* - Constructors - */
+	Ray() {}
+	Ray(const glm::vec3& origin, const glm::vec3& direction);
+
+	/* - Getters - */
+	glm::vec3 GetRayOrigin()		const { return m_Origin; }
+	glm::vec3 GetRayDirection()		const { return m_Direction; }
+
+	void SetRayOrigin(glm::vec3 origin);
+	void SetRayDirection(glm::vec3 direction);
+
+	/* - Point on the ray given a t value - */
+	glm::vec3 At(float t) const;
+
+private:
+	glm::vec3 m_Origin{ 0.0f };
+	glm::vec3 m_Direction{ 0.0f };
 };
