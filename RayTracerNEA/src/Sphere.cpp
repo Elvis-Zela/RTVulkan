1a64704377cfb5c19f2131ff4b8a45d35cdf31ad
Elvis-Zela <113384118+Elvis-Zela@users.noreply.github.com>
2022-10-01 13:31:31 +0100

    Included CHS + Refactoring
    Made the hit function only calculate if the sphere got hit and store associated t value
    Added the ClosestHitShader to the sphere
    calculates hit point normal 
    stores them + albedo to the payload

--- RayTracerNEA/src/Sphere.cpp
+++ RayTracerNEA/src/Sphere.cpp
@@ -2,15 +2,24 @@
 
 #include "Ray.h"
 
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------- Sphere Constructors -------------------------------------- */
+/* ------------------------------------------------------------------------------------------------ */
 Sphere::Sphere(glm::vec3 center, float r, glm::vec3 albedo)
 	: m_Center(center), m_Radius(r), m_Albedo(albedo)
 {}
 
 Sphere::~Sphere() {}
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
 
-//solves vector equation of a circle to see if a ray has hit it
-bool Sphere::hit(const Ray& ray, float t_min, float t_max, hit_record& rec) const
+
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------- Check If Sphere Hit -------------------------------------- */
+/* ------------------------------------------------------------------------------------------------ */
+bool Sphere::hit(const Ray& ray, float t_min, float t_max, RayPayload& rec) const
 {
+	/* - solves vector equation of a circle to see if a ray has hit it - */
 	glm::vec3 originToCenter = ray.GetRayOrigin() - m_Center;
 	auto a = glm::dot(ray.GetRayDirection(), ray.GetRayDirection());
 	auto halfB = glm::dot(originToCenter, ray.GetRayDirection());
@@ -32,14 +41,28 @@ bool Sphere::hit(const Ray& ray, float t_min, float t_max, hit_record& rec) cons
 	}
 
 	/* - Storing root - */
-	rec.closest_t = root;
-	/* - Point of intersection - */
-	rec.hit_point = ray.At(rec.closest_t);
-	/* - Storing the normal at that point, pointing out of the sphere - */
-	glm::vec3 outwardNormal = (rec.hit_point - m_Center) / m_Radius;
-	rec.set_face_normal(ray, outwardNormal);
-	rec.hit_albedo = m_Albedo;
+	rec.closestT = root;
 
 	return true;
 }
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
 
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------ Add To Payload If Hit ------------------------------------- */
+/* ------------------------------------------------------------------------------------------------ */
+void Sphere::ClosestHitShader(const Ray& ray, RayPayload& rec) const
+{
+	/* - Point of intersection - */
+	rec.hitPoint = ray.At(rec.closestT);
+
+	/* - Storing the normal at that point, pointing out of the sphere - */
+	glm::vec3 outwardNormal = (rec.hitPoint - m_Center) / m_Radius;
+	/* - Storing the normal in the scene direction - */
+	bool faceNoraml = glm::dot(outwardNormal, ray.GetRayDirection()) < 0;
+	rec.hitNormal = faceNoraml ? outwardNormal : -outwardNormal;
+
+	rec.hitAlbedo = m_Albedo;
+}
+/* ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------ */
