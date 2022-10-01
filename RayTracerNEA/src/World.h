c98c50201c5a0a8e0e4135a28dbfd24454605ac1
Elvis-Zela <113384118+Elvis-Zela@users.noreply.github.com>
2022-10-01 13:38:36 +0100

    CHS + Refactoring
    added CHS declaration + simple refactoring

--- RayTracerNEA/src/World.h
+++ RayTracerNEA/src/World.h
@@ -18,16 +18,17 @@ class World : public Hittables
 public:
 	/* - Constructors - */
 	World() {}
-	World(shared_ptr<Hittables> object) { Add(object); }
+	World(shared_ptr<Hittables> object);
 	~World();
 
 	/* - Adds a new object to the scene - */
-	void Add(shared_ptr<Hittables> object) { m_Scene.push_back(object); }
+	void Add(shared_ptr<Hittables> object);
 
 	size_t SceneSize() const { return m_Scene.size(); }
 
-	virtual bool hit(const Ray& ray, float t_min, float t_max, hit_record& rec) const override;
+	virtual bool hit(const Ray& ray, float t_min, float t_max, RayPayload& rec) const override;
+	virtual void ClosestHitShader(const Ray& ray, RayPayload& rec) const override;
 
-public:
+private:
 	std::vector<shared_ptr<Hittables>> m_Scene;
 };
