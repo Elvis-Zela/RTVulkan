91ca5f9cf55ad9c905d70dafe4c04c2947739192
Elvis-Zela <113384118+Elvis-Zela@users.noreply.github.com>
2022-10-01 13:29:04 +0100

    Added Shaders and refactoring
    Added the ray generation and miss shaders
    Included a public bounce depth to allows users to change max depth
    Included camera and world pointers to remove the need to pass the world and camera through parameters

--- RayTracerNEA/src/Renderer.h
+++ RayTracerNEA/src/Renderer.h
@@ -9,6 +9,7 @@
 
 class Camera;
 class Hittables;
+struct RayPayload;
 class Ray;
 
 class Renderer
@@ -18,17 +19,26 @@ public:
 	Renderer() = default;
 
 	void Render(const Hittables& world, const Camera& camera);
-	void SampleRender(const Hittables& world, const Camera& camera, const int sampleSize);
 	void IfResizing(uint32_t width, uint32_t height);
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
 
 /* - Private Methods - */
 private:
-	glm::vec4 TraceRay(const Ray& ray, const Hittables& world);
+	glm::vec4  RayGeneration(uint32_t x, uint32_t y);
+	void MissShader(const Ray& ray, RayPayload& payload);
+	void TraceRay(const Ray& ray, RayPayload& payload);
+
+	/* - Public Attributes for User Defined settings - */
+public:
+	int bounceDepth = 1;
 
 /* - Attributes - */
 private:
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageDataBuffer = nullptr;
+
+	/* - Pointers to the camera and world so that all methods can access them*/
+	const Camera*	  m_Camera = nullptr;
+	const Hittables*  m_World  = nullptr;
 };
