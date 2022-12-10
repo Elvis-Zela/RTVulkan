722b76a68cbf235603cdfafb87ee88453d832e3e
Elvis-Zela <113384118+Elvis-Zela@users.noreply.github.com>
2022-12-10 16:10:06 +0000

    Material Vector

--- RayTracerNEA/src/World.h
+++ RayTracerNEA/src/World.h
@@ -12,6 +12,7 @@ using std::shared_ptr;
 using std::vector;
 
 class Ray;
+class Material;
 
 class World : public Hittables
 {
@@ -22,13 +23,19 @@ public:
 	~World();
 
 	/* - Adds a new object to the scene - */
-	void Add(shared_ptr<Hittables> object);
+	int AddObject(shared_ptr<Hittables> object);
+	int AddMaterial(shared_ptr<Material> mat);
+	shared_ptr<Material> GetMat(int matIndex) const { return m_ObjectMaterials[matIndex]; }
+	shared_ptr<Hittables> GetObj(int objIndex) const { return m_SceneGeometry[objIndex]; }
 
-	size_t SceneSize() const { return m_Scene.size(); }
+	size_t SceneSize() const { return m_SceneGeometry.size(); }
 
+public:
 	virtual bool hit(const Ray& ray, float t_min, float t_max, RayPayload& rec) const override;
 	virtual void ClosestHitShader(const Ray& ray, RayPayload& rec) const override;
+	virtual int GetMatIndex() const override { return -1; }
 
-private:
-	std::vector<shared_ptr<Hittables>> m_Scene;
+public:
+	std::vector<shared_ptr<Hittables>> m_SceneGeometry;
+	std::vector<shared_ptr<Material>> m_ObjectMaterials;
 };
