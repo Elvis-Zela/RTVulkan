d91d16853dc5a85470551e1e8a781323edbbfd2b
Elvis-Zela <113384118+Elvis-Zela@users.noreply.github.com>
2022-12-10 16:06:15 +0000

    (+)Materials  (-)Albedo

--- RayTracerNEA/src/Sphere.h
+++ RayTracerNEA/src/Sphere.h
@@ -5,21 +5,23 @@
 #include "Hittables.h"
 
 class Ray;
+class Material;
 
 class Sphere : public Hittables
 {
 public:
 	/* - Constructors - */
-	Sphere(glm::vec3 center, float r, glm::vec3 albedo);
+	Sphere(glm::vec3 center, float r, int matIndex = 0);
 	~Sphere();
 
 	/* - Methods - */
 	virtual bool hit(const Ray& ray, float t_min, float t_max, RayPayload& rec) const override;
 	virtual void ClosestHitShader(const Ray& ray, RayPayload& rec) const override;
+	virtual int GetMatIndex() const override { return m_matIndex; }
 
 private:
 	/* - Attributes - */
-	glm::vec3 m_Albedo;
 	glm::vec3 m_Center;
 	float	  m_Radius;
+	int	  m_matIndex;
 };
