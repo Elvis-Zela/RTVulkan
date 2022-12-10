0d03e9c48c21d91991c2f57182afa9e4300cd1b9
Elvis-Zela <113384118+Elvis-Zela@users.noreply.github.com>
2022-12-10 16:02:14 +0000

    Added Settings
    Settings:
    Includes Used defined settings
    Added accumulator to store previous frames colour data
    ResetFrameIndex() changes the frame index of the accumulator to 1
    makes it stop accumulating frame data and resetting, essentially no accumulator

--- RayTracerNEA/src/Renderer.h
+++ RayTracerNEA/src/Renderer.h
@@ -6,22 +6,32 @@
 #include <glm/glm.hpp>
 
 #include "Utilities.h"
+#include "Materials.h"
 
 class Camera;
-class Hittables;
+class World;
 struct RayPayload;
 class Ray;
 
 class Renderer
 {
 public:
+	struct Settings
+	{
+		bool Accumulate = true;
+		int bounceDepth = 1;
+	};
+public:
 /* - Constructor - */
 	Renderer() = default;
 
-	void Render(const Hittables& world, const Camera& camera);
+	void Render(const World& world, const Camera& camera);
 	void IfResizing(uint32_t width, uint32_t height);
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
+
+	void ResetFrameIndex() { m_FrameIndex = 1; }
+	Settings& GetSettings() { return m_Settings; }
 
 /* - Private Methods - */
 private:
@@ -29,16 +39,17 @@ private:
 	void MissShader(const Ray& ray, RayPayload& payload);
 	void TraceRay(const Ray& ray, RayPayload& payload);
 
-	/* - Public Attributes for User Defined settings - */
-public:
-	int bounceDepth = 1;
-
 /* - Attributes - */
 private:
+	Settings m_Settings;
+
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageDataBuffer = nullptr;
+	glm::vec4* m_AccumulationData = nullptr;
 
-	/* - Pointers to the camera and world so that all methods can access them*/
+	uint32_t m_FrameIndex = 1;
+
+	/* - Pointers to the camera and world so that all methods can access them - */
 	const Camera*	  m_Camera = nullptr;
-	const Hittables*  m_World  = nullptr;
+	const World*	  m_World  = nullptr;
 };
