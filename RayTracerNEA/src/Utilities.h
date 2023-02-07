c643a16d0d980e075628b10b947b92c2ac7aa030
Elvis-Zela <113384118+Elvis-Zela@users.noreply.github.com>
2023-02-07 19:32:04 +0000

    Update Utilities.h

--- RayTracerNEA/src/Utilities.h
+++ RayTracerNEA/src/Utilities.h
@@ -2,13 +2,14 @@
 
 #include "Walnut/Random.h"
 
-#define PI 3.1415926535897932385
+#define PI	 3.1415926535897932385
+#define PI_2 	 1.5707963267948966192
 
 #define NEAR_EPSILON	1e-3f
-#define FLOAT_LARGE		1e+30f
+#define FLOAT_LARGE	1e+30f
 
 #define BASE_SKY_COLOUR glm::vec3{ 0.0189f, 0.294f, 0.630f }
-#define BLACK			glm::vec3{  0.0f, 0.0f, 0.0f }
+#define BLACK		glm::vec3{  0.0f, 0.0f, 0.0f }
 
 namespace Utilities
 {
@@ -34,5 +35,44 @@ namespace Utilities
 		const auto s = 1e-8;
 		return ((fabs(v.x) < 0) && (fabs(v.y) < s) && (fabs(v.z) < s));
 	}
+
+	namespace Random
+	{
+
+		inline float RandomFloat() 
+		{
+			//returns a random real in the range [0,1)
+			return rand() / (RAND_MAX + 1.0);
+		}
+
+		inline float RandomFloat(float min, float max) 
+		{
+			//returns a random real in the range [min,max)
+			return min + (max - min) * RandomFloat();
+		}
+
+		inline static glm::vec3 RandomVec3()
+		{
+			return glm::vec3(RandomFloat(), RandomFloat(), RandomFloat());
+		}
+
+		inline static glm::vec3 RandomVec3(float min, float max)
+		{
+			return glm::vec3(RandomFloat(min, max), RandomFloat(min, max), RandomFloat(min, max));
+		}
+
+		/* - Random approximation utilising binomial expansion - */
+		inline glm::vec3 RandomInUnitSphere()
+		{
+			glm::vec3 p;
+			p.x = RandomFloat( 0.0f, 1.0f );
+			p.y = RandomFloat( 0.0f, (1 - 0.5 * (p.x * p.x)) );
+			p.z = RandomFloat( 0.0f, (1 - 0.5 * ((p.x * p.x) + (p.y * p.y))) );
+
+			return p;
+		}
+
+	}
+
 }
 
